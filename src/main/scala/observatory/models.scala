f5baea054f0e596b6e993686550814edaacee704
Gerd Reiss <gerdreiss@jscriptive.com>
2017-04-08 16:06:57 +0200

    milestone 1

--- src/main/scala/observatory/models.scala
+++ src/main/scala/observatory/models.scala
@@ -1,5 +1,9 @@
 package observatory
 
+case class Station(stnId: String, wbanId: String, lat: Double, lon: Double)
+
+case class Record(stnId: String, wbanId: String, month: Int, day: Int, temp: Double)
+
 case class Location(lat: Double, lon: Double)
 
 case class Color(red: Int, green: Int, blue: Int)
--- src/test/scala/observatory/ExtractionTest.scala
+++ src/test/scala/observatory/ExtractionTest.scala
@@ -1,5 +1,7 @@
 package observatory
 
+import java.time.LocalDate
+
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
@@ -7,5 +9,37 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class ExtractionTest extends FunSuite {
 
-  
+  test("Extraction.locateTemperatures from year 2015") {
+    val result = Extraction.locateTemperatures(2015,
+      "/Users/gerdreiss/Development/99_Projects/observatory/src/main/resources/stations.csv",
+      "/Users/gerdreiss/Development/99_Projects/observatory/src/main/resources/2015.csv")
+    assert(result.nonEmpty)
+  }
+
+  test("Extraction.locationYearlyAverageRecords") {
+    val location1 = Location(76.500, 25.067)
+    val location2 = Location(70.500, 25.067)
+    val records: scala.Iterable[(LocalDate, Location, Double)] = Seq(
+      (LocalDate.of(2000, 1, 1), location1, 37.5),
+      (LocalDate.of(2000, 1, 2), location1, 37.5),
+      (LocalDate.of(2000, 1, 3), location1, 37.5),
+      (LocalDate.of(2000, 1, 4), location1, 37.5),
+      (LocalDate.of(2000, 1, 5), location2, 30.5),
+      (LocalDate.of(2000, 1, 6), location1, 37.5),
+      (LocalDate.of(2000, 1, 7), location1, 37.5),
+      (LocalDate.of(2000, 1, 8), location1, 37.5),
+      (LocalDate.of(2000, 1, 9), location2, 30.5),
+      (LocalDate.of(2001, 2, 1), location1, 37.5),
+      (LocalDate.of(2001, 2, 2), location1, 37.5),
+      (LocalDate.of(2001, 2, 3), location2, 30.5),
+      (LocalDate.of(2001, 2, 4), location1, 37.5),
+      (LocalDate.of(2001, 2, 5), location1, 37.5)
+    )
+
+    val yearlyAvg = Extraction.locationYearlyAverageRecords(records)
+    assert(yearlyAvg.nonEmpty)
+    assertResult(4)(yearlyAvg.size)
+    assertResult(37.5)(yearlyAvg.groupBy(_._1)(location1).head._2)
+    assertResult(30.5)(yearlyAvg.groupBy(_._1)(location2).head._2)
+  }
 }
